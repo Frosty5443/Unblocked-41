5c3f84d48e453824f46e6bdeaa88ddbd45a409df
Frosty5443 <liamshehane@gmail.com>
2025-11-13 21:05:44 -0500

    Add initial game data to script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+const games = [
+  {
+    id: "polytrack",
+    name: "Poly Track",
+    category: "racing",
+    url: "https://example.com/polytrack",
+    thumbLabel: "3D Racing",
+    tag: "Fast",
+    addedAt: "2025-11-01"
+  },
+  // more games...
+];
 
